21704fa6e143e3abc67f9d05dab182f2a0cfea17
dKolesnikov2003 <d.kolesnikov2003@gmail.com>
2025-06-13 07:25:50 +0300

    Упрощение bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# ---- 0. Определяем дистрибутив и пакетный менеджер -------------------------
-source /etc/os-release
-
-if grep -qi "Astra" <<<"$NAME"; then
-    PM_UPDATE="apt-get update -y"
-    PM_INSTALL="apt-get install -y"
-elif grep -qi "Elbrus" <<<"$NAME"; then       
-    PM_UPDATE="yum update -y"                 
-    PM_INSTALL="yum install -y"               
-else
-    echo "Unsupported distro: $NAME"
-    exit 1
-fi
-
-$PM_UPDATE
-$PM_INSTALL \
+apt-get update
+apt-get install -y \
     lcc lcc-devel \
     git build-essential gettext flex bison libtool autoconf automake pkg-config \
     libpcap-dev zlib1g-dev libjson-c-dev libnuma-dev libpcre2-dev \
@@ -31,13 +17,12 @@ export CFLAGS="-O2 -m64 -me2k -mvliw -vectorize -fPIC"
 export LDFLAGS="-m64"
 
 ./autogen.sh       
-
 ./configure \
     CC="$CC" \
     CFLAGS="$CFLAGS" \
     LDFLAGS="$LDFLAGS" \
     --prefix=/usr/local   \
-    --with-pic            # гарантируем правильный PIC в .so
+    --with-pic 
 
 make -j"$(nproc)"
 sudo make install
